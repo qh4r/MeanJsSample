3d48e6c53d41f92df57bc0d35b880c3ec8daa3ab
Rafał Kucharski <r.kucharski@lgbs.pl>
2015-11-11 14:32:17 +0100

    Streamlined generation of main view

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,44 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+
+		$scope.homeItems = [
+			{
+				icon: "glyphicon-user",
+				btnColor: "btn-success",
+				count: 20400,
+				description: "TOTAL CUSTOMERS"
+			},
+			{
+				icon: "glyphicon-calendar",
+				btnColor: "btn-primary",
+				count: 8300,
+				description: "UPCOMING EVENTS"
+			},
+			{
+				icon: "glyphicon-pencil",
+				btnColor: "btn-success",
+				count: 530,
+				description: "NEW CUSTOMERS"
+			},
+			{
+				icon: "glyphicon-info-sign",
+				btnColor: "btn-info",
+				count: 85000,
+				description: "EMAILS SENT"
+			},
+			{
+				icon: "glyphicon-heart-empty",
+				btnColor: "btn-warning",
+				count: 270,
+				description: "FOLLOW UPS"
+			},
+			{
+				icon: "glyphicon-flag",
+				btnColor: "btn-danger",
+				count: 350,
+				description: "REFFEREALS TO MODERATE"
+			}
+		]
 	}
 ]);
