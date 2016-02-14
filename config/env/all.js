194fd7edd4bfcd39cf7060fa09c1c2b350d99f68
Rafał Kucharski <r.kucharski@lgbs.pl>
2016-02-14 14:08:45 +0100

    Added create customer

--- config/env/all.js
+++ config/env/all.js
@@ -29,7 +29,8 @@ module.exports = {
 			]
 		},
 		css: [
-			'public/modules/**/css/*.css'
+			'public/modules/**/css/*.css',
+			'public/modules/customers/css/customers.css'
 		],
 		js: [
 			'public/config.js',
